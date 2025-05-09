90d558fa0502593efead80ba4e31ceac11a9fafc
riveraE4 <riverae4@tcnj.edu>
2025-05-09 21:23:04 +0000

    5:22 changes, please check for CP1 and CP2

--- Project4/chat_client.c
+++ Project4/chat_client.c
@@ -5,81 +5,146 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <netdb.h>
+#include <pthread.h>
+#include <errno.h>
+#include <signal.h>
 
 #define PORT_NUM 1004
 #define NAME_LEN 32
 #define MSG_LEN 512
 
+int sockfd; // Global socket descriptor for cleanup in signal handler
+int username_provided = 0; // Flag to track if username has been set
+
 void error(const char *msg)
 {
-	perror(msg);
-	exit(0);
+    perror(msg);
+    exit(0);
+}
+
+// Signal handler for Ctrl+C
+void handle_signal(int sig) 
+{
+    printf("\nDisconnecting from server...\n");
+    close(sockfd);
+    exit(0);
+}
+
+// Thread function to continuously receive messages from server
+void* receive_messages(void* arg) 
+{
+    char buffer[MSG_LEN];
+    int n;
+    
+    while (1) {
+        memset(buffer, 0, sizeof(buffer));
+        n = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
+        
+        if (n <= 0) {
+            if (n < 0) {
+                fprintf(stderr, "Error receiving message: %s\n", strerror(errno));
+            } else {
+                printf("Server disconnected\n");
+            }
+            close(sockfd);
+            exit(1);
+        }
+        
+        buffer[n] = '\0';
+        printf("%s", buffer); // Print the received message
+        
+        // Check if this is the username prompt
+        if (!username_provided && strstr(buffer, "Type username:") != NULL) {
+            printf("Enter your username: ");
+            fflush(stdout);
+        } else {
+            // For regular messages, add a prompt for the next message
+            if (username_provided) {
+                printf("> ");
+                fflush(stdout);
+            }
+        }
+    }
+    
+    return NULL;
 }
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2) error("Please specify hostname");
-
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) error("ERROR opening socket");
-
-	struct hostent* server = gethostbyname(argv[1]);
-	if (server == NULL) error("ERROR, no such host");
-
-	struct sockaddr_in serv_addr;
-	socklen_t slen = sizeof(serv_addr);
-	memset((char*) &serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	memcpy((char*)server->h_addr, 
-			(char*)&serv_addr.sin_addr.s_addr,
-			server->h_length);
-	serv_addr.sin_port = htons(PORT_NUM);
-
-	int status = connect(sockfd, 
-			(struct sockaddr *) &serv_addr, slen);
-	if (status < 0) error("ERROR connecting");
-
-
-	char buffer[256];
-	int n;
-
-	memset(buffer, 0, sizeof(buffer));
-	n = recv(sockfd, buffer, sizeof(buffer)-1, 0);
-	if (n < 0) error("error msg");
-	buffer[n] ='\0';
-	printf("%s", buffer);
-
-	memset(buffer, 0, sizeof(buffer));
-	if (!fgets(buffer, 255, stdin)) exit(0);
-	buffer[strcspn(buffer, "\n")] ='\0';
-	n = send(sockfd, buffer, strlen(buffer), 0);
-	if (n < 0) error("username error");
-
-	while (1) {
-		printf("Please enter the message: ");
-		memset(buffer, 0, 256);
-		fgets(buffer, 255, stdin);
-		
-		// have message of length 1. so, you want to remove that
-		// since fgets() considers '\n' as a valid character,
-		// even if you type nothing and enter, you will still
-		if (strlen(buffer) == 1) buffer[0] = '\0';
-
-		n = send(sockfd, buffer, strlen(buffer), 0);
-		if (n < 0) error("ERROR writing to socket");
-
-		if (n == 0) break; // we stop transmission when user type empty string
-
-		memset(buffer, 0, 256);
-		n = recv(sockfd, buffer, 255, 0);
-		if (n < 0) error("ERROR reading from socket");
-
-		printf("Message from server: %s\n", buffer);
-	}
-
-	close(sockfd);
-
-	return 0;
+    // Set up signal handler for graceful exit
+    signal(SIGINT, handle_signal);
+    
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s hostname\n", argv[0]);
+        exit(1);
+    }
+    
+    printf("Try connecting to %s...\n", argv[1]);
+    
+    // Create socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) error("ERROR opening socket");
+    
+    // Get server information
+    struct hostent* server = gethostbyname(argv[1]);
+    if (server == NULL) error("ERROR, no such host");
+    
+    // Set up server address
+    struct sockaddr_in serv_addr;
+    socklen_t slen = sizeof(serv_addr);
+    memset((char*) &serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    memcpy((char*)&serv_addr.sin_addr.s_addr,
+           (char*)server->h_addr,
+           server->h_length);
+    serv_addr.sin_port = htons(PORT_NUM);
+    
+    // Connect to server
+    int status = connect(sockfd, (struct sockaddr *) &serv_addr, slen);
+    if (status < 0) error("ERROR connecting");
+    
+    printf("Connected to server. Waiting for welcome message...\n");
+    
+    // Start thread to receive messages
+    pthread_t recv_thread;
+    if (pthread_create(&recv_thread, NULL, receive_messages, NULL) != 0) {
+        error("ERROR creating receiver thread");
+    }
+    
+    // Main loop for sending messages
+    char buffer[MSG_LEN];
+    int n;
+    
+    while (1) {
+        memset(buffer, 0, sizeof(buffer));
+        
+        // Read input from user
+        if (!fgets(buffer, sizeof(buffer) - 1, stdin)) {
+            break; // Exit on EOF
+        }
+        
+        // Remove trailing newline if present
+        size_t len = strlen(buffer);
+        if (len > 0 && buffer[len-1] == '\n') {
+            buffer[len-1] = '\0';
+            len--;
+        }
+        
+        // Track when username is provided
+        if (!username_provided) {
+            username_provided = 1;
+        }
+        
+        // Send message to server
+        n = send(sockfd, buffer, strlen(buffer), 0);
+        if (n < 0) {
+            fprintf(stderr, "ERROR sending message: %s\n", strerror(errno));
+            break;
+        }
+    }
+    
+    // Clean up
+    close(sockfd);
+    return 0;
 }
-
--- Project4/chat_server.c
+++ Project4/chat_server.c
@@ -7,7 +7,10 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <pthread.h>
-
+#include <errno.h>
+#include <time.h>
+#include <signal.h>
+#include <fcntl.h>
 
 #define PORT_NUM 1004
 #define NAME_LEN 32
@@ -16,191 +19,251 @@
 
 void error(const char *msg)
 {
-	perror(msg);
-	exit(1);
+    perror(msg);
+    exit(1);
 }
 
 typedef struct _ThreadArgs {
-	int clisockfd; 
-	char name[NAME_LEN]; // storing of username
-	int color; // Ansi color code
+    int clisockfd; 
+    char name[NAME_LEN]; // storing of username
+    int color; // Ansi color code
+    int valid; // Flag to track if socket is valid
 } ThreadArgs;
 
 //client list and mutex
 static ThreadArgs *clients[MAX_CLIENTS] = {0};
 static pthread_mutex_t clients_mtx = PTHREAD_MUTEX_INITIALIZER;
 
+// Ignore SIGPIPE signals to prevent program termination
+void setup_signal_handler() {
+    struct sigaction sa;
+    sa.sa_handler = SIG_IGN;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGPIPE, &sa, NULL);
+}
+
+// Function to safely send data to a socket
+int safe_send(int sockfd, const char *msg, size_t len) {
+    if (sockfd < 0) return -1;
+    
+    // Try to send with NOSIGNAL to prevent SIGPIPE
+    int ret = send(sockfd, msg, len, MSG_NOSIGNAL);
+    if (ret < 0) {
+        printf("Send error: %s (errno=%d)\n", strerror(errno), errno);
+    }
+    return ret;
+}
+
+// Print connected users (for server logs)
+void print_connected_users() 
+{
+    printf("Connected Users: ");
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i] && clients[i]->valid) printf(" %s", clients[i]->name);
+    }
+    printf("\n");
+}
+
 void broadcast(const char *msg) //broadcasting a message to all clients
 {
-	pthread_mutex_lock(&clients_mtx);
-	for (int i = 0; i < MAX_CLIENTS; ++i) {
-		if (clients[i]) {
-			send(clients[i]->clisockfd, msg, strlen(msg), 0);
-		}
-	}
-	pthread_mutex_unlock(&clients_mtx);
+    pthread_mutex_lock(&clients_mtx);
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (clients[i] && clients[i]->valid) {
+            // Try to send, if it fails, mark this client for removal
+            if (safe_send(clients[i]->clisockfd, msg, strlen(msg)) < 0) {
+                printf("Failed to send to client %s\n", clients[i]->name);
+                clients[i]->valid = 0;
+            }
+        }
+    }
+    
+    // Clean up invalid clients
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (clients[i] && !clients[i]->valid) {
+            printf("Cleaning up invalid client: %s\n", clients[i]->name);
+            close(clients[i]->clisockfd);
+            free(clients[i]);
+            clients[i] = NULL;
+        }
+    }
+    pthread_mutex_unlock(&clients_mtx);
 }
 
 void register_client(ThreadArgs *c)
 {
-	pthread_mutex_lock(&clients_mtx);
-	for (int i = 0; i < MAX_CLIENTS; ++i) {
-		if (!clients[i]) {
-			clients[i] = c;
-			break;
-		}
-	}
-	pthread_mutex_unlock(&clients_mtx);
-	printf("Connected Users: ");
-	for ( int i = 0; i < MAX_CLIENTS; i++) {
-		if (clients[i]) printf(" %s", clients[i]->name);
-	}
-	printf("\n");
+    pthread_mutex_lock(&clients_mtx);
+    int added = 0;
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (!clients[i]) {
+            clients[i] = c;
+            added = 1;
+            break;
+        }
+    }
+    pthread_mutex_unlock(&clients_mtx);
+    
+    if (!added) {
+        printf("WARNING: Maximum clients reached, couldn't add %s\n", c->name);
+        // Send a message to the client and close connection
+        const char *msg = "Server is full, try again later.\n";
+        safe_send(c->clisockfd, msg, strlen(msg));
+        close(c->clisockfd);
+        free(c);
+        return;
+    }
+    
+    print_connected_users();
 }
 
 void deregister_client(ThreadArgs *c)
 {
-	pthread_mutex_lock(&clients_mtx);
-	for (int i = 0; i < MAX_CLIENTS; ++i) {
-		if (clients[i] == c) {
-			clients[i] = NULL;
-			break;
-		}
-	}
-	pthread_mutex_unlock(&clients_mtx);
-	printf("Connected Users: ");
-	for ( int i = 0; i < MAX_CLIENTS; i++) {
-		if (clients[i]) printf(" %s", clients[i]->name);
-	}
-	printf("\n");
+    pthread_mutex_lock(&clients_mtx);
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        if (clients[i] == c) {
+            clients[i] = NULL;
+            break;
+        }
+    }
+    pthread_mutex_unlock(&clients_mtx);
+    print_connected_users();
 }
 
 void *handle_client(void *arg)
 {
-	pthread_detach(pthread_self());
-	ThreadArgs *cli = (ThreadArgs *)arg;
-	char buf[MSG_LEN];
-	int n;
+    pthread_detach(pthread_self());
+    ThreadArgs *cli = (ThreadArgs *)arg;
+    char buf[MSG_LEN];
+    int n;
 
-	// recieve usernames
-	const char *ask = "Type username: ";
-	send(cli->clisockfd, ask, strlen(ask), 0);
-	n = recv(cli->clisockfd, buf, NAME_LEN - 1, 0);
-	if (n <= 0) {
-		close(cli->clisockfd);
-		free(cli);
-		return NULL;
-	}
-	buf[n] = '\0';
-	strncpy(cli->name, buf, NAME_LEN);
+    // Initialize client
+    cli->valid = 1;
+    memset(cli->name, 0, NAME_LEN);
+    strcpy(cli->name, "Anonymous"); // Default name in case user disconnects early
 
-	//color assign
-	cli->color = 31 + (rand() % 7);
+    // Send welcome message and request username
+    const char *ask = "Type username: ";
+    if (safe_send(cli->clisockfd, ask, strlen(ask)) < 0) {
+        printf("Error sending username prompt\n");
+        close(cli->clisockfd);
+        free(cli);
+        return NULL;
+    }
+    
+    // Wait for username
+    n = recv(cli->clisockfd, buf, NAME_LEN - 1, 0);
+    if (n <= 0) {
+        printf("Client disconnected during username entry\n");
+        close(cli->clisockfd);
+        free(cli);
+        return NULL;
+    }
+    
+    // Process username
+    buf[n] = '\0';
+    if (strlen(buf) > 0) {
+        // Remove any trailing newline
+        if (buf[strlen(buf)-1] == '\n') {
+            buf[strlen(buf)-1] = '\0';
+        }
+        strncpy(cli->name, buf, NAME_LEN - 1);
+        cli->name[NAME_LEN - 1] = '\0'; // Ensure null termination
+    }
+    
+    // Assign color and register
+    cli->color = 31 + (rand() % 7);
+    register_client(cli);
+    
+    // Create welcome message with a newline at the end
+    snprintf(buf, sizeof(buf), "\033[1;%dm%s joined the chat!\033[0m\n", cli->color, cli->name);
+    broadcast(buf);
 
-	//broadcast join
-	register_client(cli);
-	snprintf(buf, sizeof(buf), "\033[1;%dm%s joined the chat!\n\033[0m", cli->color, cli->name);
-	broadcast(buf);
-
-	while ((n = recv(cli->clisockfd, buf, MSG_LEN - 1, 0)) > 0) {
-		buf[n] = '\0';
-		char out[MSG_LEN + NAME_LEN + 16];
-		snprintf(out, sizeof(out), "\033[1;%dm[%s]\033[0m: %s", cli->color, cli->name, buf);
-		broadcast(out);
-	}
-	snprintf(buf, sizeof(buf), "\033[1;%dm%s left the chat!\n\033[0m", cli->color, cli->name);
-	broadcast(buf);
-	deregister_client(cli);
-
-	close(cli->clisockfd);
-	free(cli);
-	return NULL;
-
-
+    // Main message processing loop
+    while (1) {
+        n = recv(cli->clisockfd, buf, MSG_LEN - 1, 0);
+        if (n <= 0) {
+            if (n < 0) {
+                printf("Error receiving from %s: %s\n", cli->name, strerror(errno));
+            } else {
+                printf("Client %s disconnected\n", cli->name);
+            }
+            break;
+        }
+        
+        buf[n] = '\0';
+        
+        // Add newline if not present
+        char out[MSG_LEN + NAME_LEN + 32];
+        if (buf[strlen(buf)-1] != '\n') {
+            snprintf(out, sizeof(out), "\033[1;%dm[%s]\033[0m: %s\n", cli->color, cli->name, buf);
+        } else {
+            snprintf(out, sizeof(out), "\033[1;%dm[%s]\033[0m: %s", cli->color, cli->name, buf);
+        }
+        
+        broadcast(out);
+    }
+    
+    // Announce departure
+    snprintf(buf, sizeof(buf), "\033[1;%dm%s left the chat!\033[0m\n", cli->color, cli->name);
+    broadcast(buf);
+    
+    // Clean up this client
+    deregister_client(cli);
+    close(cli->clisockfd);
+    free(cli);
+    return NULL;
 }
-
-// void* thread_main(void* args)
-// {
-// 	// make sure thread resources are deallocated upon return
-// 	pthread_detach(pthread_self());
-
-// 	// get socket descriptor from argument
-// 	int clisockfd = ((ThreadArgs*) args)->clisockfd;
-// 	free(args);
-
-// 	//-------------------------------
-// 	// Now, we receive/send messages
-// 	char buffer[256];
-// 	int nsen, nrcv;
-
-// 	nrcv = recv(clisockfd, buffer, 256, 0);
-// 	if (nrcv < 0) error("ERROR recv() failed");
-
-// 	while (nrcv > 0) {
-// 		nsen = send(clisockfd, buffer, nrcv, 0);
-// 		if (nsen != nrcv) error("ERROR send() failed");
-
-// 		nrcv = recv(clisockfd, buffer, 256, 0);
-// 		if (nrcv < 0) error("ERROR recv() failed");
-// 	}
-
-// 	close(clisockfd);
-// 	//-------------------------------
-
-// 	return NULL;
-// }
 
 int main(int argc, char *argv[])
 {
-	srand(time(NULL));
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) error("ERROR opening socket");
+    // Set up signal handler
+    setup_signal_handler();
+    
+    srand(time(NULL));
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) error("ERROR opening socket");
+    
+    // Set socket option to reuse address
+    int opt = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        error("ERROR setting socket options");
+    }
 
-	struct sockaddr_in serv_addr;
-	socklen_t slen = sizeof(serv_addr);
-	memset((char*) &serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;	
-	serv_addr.sin_port = htons(PORT_NUM);
+    struct sockaddr_in serv_addr;
+    socklen_t slen = sizeof(serv_addr);
+    memset((char*) &serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;    
+    serv_addr.sin_port = htons(PORT_NUM);
 
-	int status = bind(sockfd, 
-			(struct sockaddr*) &serv_addr, slen);
-	if (status < 0) error("ERROR on binding");
+    int status = bind(sockfd, 
+            (struct sockaddr*) &serv_addr, slen);
+    if (status < 0) error("ERROR on binding");
 
-	listen(sockfd, 5);
+    listen(sockfd, 5);
+    printf("Chat server started on port %d\n", PORT_NUM);
 
-	while(1) {
-		struct sockaddr_in cli_addr;
-		socklen_t clen = sizeof(cli_addr);
-		int newsockfd = accept(sockfd, 
-			(struct sockaddr *) &cli_addr, &clen);
-		if (newsockfd < 0) error("ERROR on accept");
+    while(1) {
+        struct sockaddr_in cli_addr;
+        socklen_t clen = sizeof(cli_addr);
+        int newsockfd = accept(sockfd, 
+            (struct sockaddr *) &cli_addr, &clen);
+        if (newsockfd < 0) error("ERROR on accept");
 
-		printf("Connected: %s\n", inet_ntoa(cli_addr.sin_addr));
+        printf("Connected: %s\n", inet_ntoa(cli_addr.sin_addr));
 
-		ThreadArgs *cli = malloc(sizeof(ThreadArgs));
-		if (!cli) error ("malloc");
-		cli->clisockfd = newsockfd;
+        ThreadArgs *cli = malloc(sizeof(ThreadArgs));
+        if (!cli) error("malloc");
+        cli->clisockfd = newsockfd;
 
-		pthread_t tid;
-		if (pthread_create(&tid, NULL, handle_client, cli)!=0)
-		{
-			perror("pthread_create");
-			close(newsockfd);
-			free(cli);
-		}
+        pthread_t tid;
+        if (pthread_create(&tid, NULL, handle_client, cli)!=0) {
+            perror("pthread_create");
+            close(newsockfd);
+            free(cli);
+        }
+    }
 
-		// prepare ThreadArgs structure to pass client socket
-		// ThreadArgs* args = (ThreadArgs*) malloc(sizeof(ThreadArgs));
-		// if (args == NULL) error("ERROR creating thread argument");
-		
-		// args->clisockfd = newsockfd;
-
-		// pthread_t tid;
-		// if (pthread_create(&tid, NULL, thread_main, (void*) args) != 0) error("ERROR creating a new thread");
-	}
-
-	close(sockfd);
-	return 0; 
+    close(sockfd);
+    return 0; 
 }
-
